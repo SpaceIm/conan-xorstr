bbccd9a47a55dfdf9b389af8f271a50f473a8e40
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-14 10:10:12 +0200

    check compiler

--- conanfile.py
+++ conanfile.py
@@ -19,30 +19,30 @@ class XorstrConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    # @property
-    # def _compilers_minimum_version(self):
-    #     return {
-    #         "Visual Studio": "15.7",
-    #         "gcc": "7",
-    #         "clang": "4.0",
-    #         "apple-clang": "9.1"
-    #     }
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "16",
+            "gcc": "7",
+            "clang": "5.0",
+            "apple-clang": "9.1"
+        }
 
-    # def validate(self):
-    #     if self.settings.compiler.get_safe("cppstd"):
-    #         tools.check_min_cppstd(self, 17)
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 17)
 
-    #     def lazy_lt_semver(v1, v2):
-    #         lv1 = [int(v) for v in v1.split(".")]
-    #         lv2 = [int(v) for v in v2.split(".")]
-    #         min_length = min(len(lv1), len(lv2))
-    #         return lv1[:min_length] < lv2[:min_length]
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
 
-    #     minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-    #     if not minimum_version:
-    #         self.output.warn("{} {} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name, self.version))
-    #     elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
-    #         raise ConanInvalidConfiguration("{} {} requires C++17, which your compiler does not support.".format(self.name, self.version))
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if not minimum_version:
+            self.output.warn("{} {} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name, self.version))
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("{} {} requires C++17, which your compiler does not support.".format(self.name, self.version))
 
     def package_id(self):
         self.info.header_only()
